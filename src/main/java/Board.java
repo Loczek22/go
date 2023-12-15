8d90e285fa5db186f8d6515f81669a53a892c7c7
Aleksandra Czarniecka <a.czarniecka03@gmail.com>
2023-12-15 17:34:13 +0100

    Stworzenie klas SingleStone, StonesGroup, Board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,18 +1,59 @@
 public class Board {
-    private int BoardSize;
-    private Stone [][] stones;
+    private int boardSize; //19, 13, 9
+    private SingleStone [][] stones;
 
     public Board(int boardSize) {
-        BoardSize = boardSize;
-        this.stones = new Stone[BoardSize][BoardSize];
+        boardSize = boardSize;
+        this.stones = new SingleStone[boardSize][boardSize];
+        resetAll();
+    }
+
+    public int getBoardSize() {
+        return boardSize;
+    }
+
+    // resetowanie planszy
+    public void resetAll() {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                stones[i][j] = null;
+            }
+        }
+    }
+
+    public SingleStone getStoneAt(int x, int y) {
+        if (isValidCoordinate(x, y)) {
+            return stones[x][y];
+        }
+        return null;
+    }
+
+    public void placeStone(SingleStone stone) {
+        int x = stone.getX();
+        int y = stone.getY();
+
+        if (isValidCoordinate(x, y)) {
+            stones[x][y] = stone;
+        }
+    }
+    public void removeStone(int x, int y) {
+        if (isValidCoordinate(x, y)) {
+            stones[x][y] = null;
+        }
+    }
+
+    // obsługa wyjścia poza planszę
+    private boolean isValidCoordinate(int x, int y) {
+        return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
     }
 
     // czy ruch jest legalny
     public boolean isValidMove(int x, int y) {
+        // TODO: logika poprawnośći ruchu
         return true;
     }
 
-    public void stoneMove(StoneColor color, int x, int y) {
-        stones[x][y] = new Stone(color, x, y);
+    public boolean isEmpty(int x, int y) {
+        return isValidCoordinate(x, y) && stones[x][y] == null;
     }
 }
--- src/main/java/SingleStone.java
+++ src/main/java/SingleStone.java
@@ -1,13 +1,17 @@
-public class Stone {
+public class SingleStone {
     private StoneColor color;
     private int x, y; // współrzędne kamienia na planszy
-    private boolean isAlive;
 
-    public Stone(StoneColor color, int x, int y) {
+    public SingleStone(StoneColor color, int x, int y) {
         this.color = color;
         this.x = x;
         this.y = y;
-        this.isAlive = true;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
     }
     public StoneColor getColor() {
         return color;
@@ -15,13 +19,5 @@ public class Stone {
     // sprawdzenie czy kamień ma oddechy
     public boolean hasBreaths() {
         return true;
-    }
-    // ustawienie śmierci kamienia
-    public void setDead() {
-        isAlive = false;
-    }
-    // sprawdzenie czy kamień jest żywy
-    public boolean isAlive() {
-        return isAlive;
     }
 }
